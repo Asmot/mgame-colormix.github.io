2f0e0d480698d69e7c03a5f0c83bdcac69ccb61c
zxy <247176265@qq.com>
2024-04-23 09:44:29 +0800

    feat: init proj

---
+++ assets/ui_resources/index.js
@@ -0,0 +1,20 @@
+System.register("chunks:///_virtual/ui_resources",[],(function(){return{execute:function(){}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/ui_resources', 'chunks:///_virtual/ui_resources'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
